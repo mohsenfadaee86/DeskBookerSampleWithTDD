a94df70dbbce1d57adf659e744e9ab62db5b2085
Mohsen Fadaee <mohsen4us@gmail.com>
2022-02-11 13:56:05 +0330

    Add project files.

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DeskBooker.Core.Processor
+{
+    internal class DeskBookingRequestProcessor
+    {
+        public DeskBookingRequestProcessor()
+        {
+        }
+
+        internal DeskBookingResult BookDesk(DeskbookingRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DeskBooker.Core.Tests/Processor/DeskbookingRequestProcessorTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskbookingRequestProcessorTests
+    {
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestVlaues()
+        {
+
+            //Arrange
+            var request = new DeskbookingRequest
+            {
+                FirstName = "Mohsen",
+                LastName = "Fadaee",
+                Email = "Mohsen4us@gmail.com",
+                Date = new DateTime(2022, 02, 11)
+            };
+            var processor = new DeskBookingRequestProcessor();
+
+            //Act
+            DeskBookingResult result= processor.BookDesk(request);
+
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+
+        }
+    }
+}
