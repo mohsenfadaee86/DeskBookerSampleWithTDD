88fda11b796e1fe4ed7fdc3a59cbc47bc21a1faf
Mohsen Fadaee <mohsen4us@gmail.com>
2022-02-11 16:03:44 +0330

    Red - wrtie test - should not save desk booking when not available

--- DeskBooker.Core.Tests/Processor/DeskbookingRequestProcessorTests.cs
+++ DeskBooker.Core.Tests/Processor/DeskbookingRequestProcessorTests.cs
@@ -2,6 +2,7 @@
 using DeskBooker.Core.Domain;
 using Moq;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace DeskBooker.Core.Processor
@@ -9,7 +10,9 @@ namespace DeskBooker.Core.Processor
     public class DeskbookingRequestProcessorTests
     {
         private readonly DeskbookingRequest _request;
+        private readonly List<Desk> _availableDesks;
         private readonly Mock<IDeskBookingRepository> _deskBookingRepositoryMock;
+        private readonly Mock<IDeskRepository> _deskRepositoryMock;
         private readonly DeskBookingRequestProcessor _processor;
 
         public DeskbookingRequestProcessorTests()
@@ -22,8 +25,14 @@ namespace DeskBooker.Core.Processor
                 Date = new DateTime(2022, 02, 11)
             };
 
+            _availableDesks = new List<Desk> { new Desk() };
+
             _deskBookingRepositoryMock = new Mock<IDeskBookingRepository>();
-            _processor = new DeskBookingRequestProcessor(_deskBookingRepositoryMock.Object);
+            _deskRepositoryMock = new Mock<IDeskRepository>();
+
+            _deskRepositoryMock.Setup(x => x.GetAvailableDesks(_request.Date)).Returns(_availableDesks);
+
+            _processor = new DeskBookingRequestProcessor(_deskBookingRepositoryMock.Object, _deskRepositoryMock.Object);
         }
         [Fact]
         public void ShouldReturnDeskBookingResultWithRequestVlaues()
@@ -79,6 +88,17 @@ namespace DeskBooker.Core.Processor
             Assert.Equal(_request.LastName, savedDeskBooking.LastName);
             Assert.Equal(_request.Email, savedDeskBooking.Email);
             Assert.Equal(_request.Date, savedDeskBooking.Date);
+        }
+
+
+        [Fact]
+        public void ShouldNotSaveDeskBookingIfNotAvailable()
+        {
+            _availableDesks.Clear();
+            _processor.BookDesk(_request);
+
+
+            _deskBookingRepositoryMock.Verify(x => x.Save(It.IsAny<DeskBooking>()), Times.Never);
         }
     }
 }
