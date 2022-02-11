0e35e62c99e4c1830154d590866f1d0e1e3b704c
Mohsen Fadaee <mohsen4us@gmail.com>
2022-02-11 14:15:04 +0330

    Green - Return Throw Exception If Request is null

--- DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
+++ DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -10,6 +10,10 @@ namespace DeskBooker.Core.Processor
 
         public DeskBookingResult BookDesk(DeskbookingRequest request)
         {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
             return new DeskBookingResult
             {
                 FirstName = request.FirstName,
